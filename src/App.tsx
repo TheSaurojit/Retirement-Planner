9106660466a469e7ed27fba5e608cb2dcd017e4b
TheSaurojit <saurojitkarmakar947@gmail.com>
2025-05-17 19:09:58 +0530

    retirement

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Investments from "./pages/Investments";
+import News from "./pages/News";
+import StockMarket from "./pages/StockMarket";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/investments" element={<Investments />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/stock-market" element={<StockMarket />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
